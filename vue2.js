94c46a48dd332601d95f7569f607cc5bec25e032
Peter Lavrov <dotaplay4fun@gmail.com>
2021-02-12 15:34:16 +0200

    добавление формата для цены

---
+++ vue2.js
@@ -0,0 +1,17 @@
+var vm = new Vue (
+  {
+    el: '#app',
+    data: {
+      isActive: true,
+      error: null
+    },
+    computed: {
+      classObject: function () {
+        return {
+          active: this.isActive && !this.error,
+          'text-danger': this.error && this.error.type === 'fatal'
+        }
+      }
+    }
+  }
+)
